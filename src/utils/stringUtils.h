76dda5b7be8a0be0948f2d39a544d250a7115c5e
LittleSwift <60036604+LittleSwift@users.noreply.github.com>
2025-10-08 15:43:08 +0800

    feat: Using sha256 for mangled name to avoid file name too long

--- src/utils/stringUtils.h
+++ src/utils/stringUtils.h
@@ -1,5 +1,7 @@
 #pragma once
+#include <sstream>
 #include <string>
+#include <openssl/sha.h>
 
 inline std::string zerosString(int n) {
     if (n <= 0) {
@@ -12,7 +14,26 @@ inline std::string zerosString(int n) {
     return result;
 }
 
+inline std::string sha256Hash(const std::string& input) {
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256(reinterpret_cast<const unsigned char*>(input.c_str()), input.size(), hash);
+
+    std::stringstream ss;
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; ++i) {
+        ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
+    }
+    return ss.str();
+}
+
+inline std::string safeName(const std::string& name) {
+    if (name.rfind("_Z", 0) != 0) {
+        return name;
+    }
+    return sha256Hash(name);
+}
+
 inline std::string standardizeName(std::string name) {
+    name = safeName(name);
     std::ranges::transform(name, name.begin(),
                            [](const unsigned char c){ return std::tolower(c); });
     return name;
